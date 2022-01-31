4ea7b9f240194ab7aaeaf8d181c52c71fb0cfea4
nmcanall <nmcanall@citadel.edu>
2022-01-30 20:35:49 -0600

    footer styling and responsive queries

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 function Footer() {
     return (
         <div className="footer">
-            <p>facebook</p>
-            <p>instagram</p>
-            <p>github</p>
+            <a href="https://www.facebook.com/nathan.mcanally.58" className="fa fa-facebook"> </a>
+            <a href="https://www.instagram.com/nathanmcanally/" className="fa fa-instagram"> </a>
+            <a href="https://github.com/nmcanall" className="fa fa-github"> </a>
         </div>
     )
 }
