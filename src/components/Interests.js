dd5942e6552ba355e55e3039218a84a2badaaa70
nmcanall <nmcanall@citadel.edu>
2022-02-11 16:26:26 -0600

    prepare for deploy

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,7 +2,7 @@ function Interests() {
     return (
         <div className="section">
             <h3 className="section-title">Interests</h3>
-            <p className="section-text">Within academics, I have a love of mathematical research and related programming. To expand my programming skills, I branched into software and web development. Outside of the office, I spend my time traveling with my wife in our self-built camper van. Popular destinations are climbing boulders, mountain biking/unicycling trails, mountain peaks, and whitewater rivers.</p>
+            <p className="section-text">Within academics, I have a love of mathematical research and related programming. To expand my programming skills, I branched into software and web development. Outside of the office, I spend my time traveling with my wife in our self-built camper van. Popular destinations are climbing crags, mountain biking/unicycling trails, mountain peaks, and whitewater rivers.</p>
         </div>
     )
 }
