ad7d440063077f4a0d5abe6e7e758432be3166d4
nmcanall <nmcanall@citadel.edu>
2022-02-22 14:44:28 -0600

    added link to travel-journal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 function Footer() {
     return (
         <div className="footer">
-            <a href="https://www.facebook.com/nathan.mcanally.58" className="fa fa-facebook"> </a>
-            <a href="https://www.instagram.com/nathanmcanally/" className="fa fa-instagram"> </a>
-            <a href="https://github.com/nmcanall" className="fa fa-github"> </a>
+            <a href="https://www.facebook.com/nathan.mcanally.58" className="fa fa-facebook" rel="noreferrer" target="_blank"> </a>
+            <a href="https://www.instagram.com/nathanmcanally/" className="fa fa-instagram" rel="noreferrer" target="_blank"> </a>
+            <a href="https://github.com/nmcanall" className="fa fa-github" rel="noreferrer" target="_blank"> </a>
+            <a href="https://nmcanall.github.io/travel-journal/" className="fa fa-bicycle" rel="noreferrer" target="_blank"> </a>
         </div>
     )
 }
